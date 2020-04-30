d6c454ceb6ed8413865247cc0ecd3b8f04970676
ErikaDelaRosa <erika-delarosa@hotmail.com>
2020-04-30 13:21:28 +0200

    daily exercises

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-if ( ($(window).height() + 100) < $(document).height() ) {
+/*if ( ($(window).height() + 100) < $(document).height() ) {
     $('#top-link-block').removeClass('hidden').affix({
         // how far to scroll down before link "slides" into view
         offset: {top:100}
     });
-}
+}*/
+
+function myFunction() {
+    var element = document.getElementById("#toystory");
+    element.classList.toggle("movie-content");
+ }
